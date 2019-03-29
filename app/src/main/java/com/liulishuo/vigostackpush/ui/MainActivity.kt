105d37d938b85b0c83736c52c97f154c62d8c926
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-03-29 17:08:26 +0800

    Initial commit

---
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.liulishuo.vigostackpush.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.liulishuo.vigostackpush.R
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private val tag = "vigroid"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { view ->
+            Observable
+                    .just("Hello")
+                    .doOnNext { logInfo("doOnNext(): $it") }
+                    .doAfterNext { logInfo("doAfterNext(): $it") }
+                    .doOnComplete { logInfo("doOnComplete()") }
+                    .doOnSubscribe { logInfo("doOnSubscribe(): ${it.isDisposed}") }
+                    .doAfterTerminate { logInfo("doAfterTerminate()") }
+                    .doFinally { logInfo("doFinally():") }
+                    .doOnEach { logInfo("doOnEach(): $it") }
+                    .doOnLifecycle({ logInfo("doOnLifecycle(): ${it.isDisposed}") }, { logInfo("doOnLifecycle(): run") })
+                    .map { "$it world" }
+                    .subscribe({ logInfo("收到 $it") }, {}, { logInfo("received onCompleted") }, { logInfo("received onSubscribe") })
+        }
+    }
+
+    private fun logInfo(info: String) {
+        Log.i("vigo", info)
+    }
+}
