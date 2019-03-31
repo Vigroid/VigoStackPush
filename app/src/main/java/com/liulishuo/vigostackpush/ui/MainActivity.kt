5906a55db220339f5f0f534c98e2e1719930a60a
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-03-31 16:58:21 +0800

    repeat操作符基本操作

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -5,39 +5,24 @@ import android.os.Bundle
 import android.util.Log
 import com.liulishuo.vigostackpush.R
 import io.reactivex.Observable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
-
-    class MyCallable : Callable<String> {
-        override fun call(): String {
-            Thread.sleep(5000)
-            return "ojbk"
-        }
-
-    }
-
-    private val threadPool = Executors.newCachedThreadPool()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         start.setOnClickListener {
-            val stringFuture = threadPool.submit(MyCallable())
-
             Observable
-                    .fromFuture(stringFuture, 2, TimeUnit.SECONDS)
+                    .just("heihei")
+                    .repeat(3)
                     .subscribe({ s -> logInfo(s) }, { t -> logInfo(t.toString()) }, { logInfo("on completed") })
         }
     }
 
-    private fun logInfo(info: String?, tag: String = "abc") {
+    private fun logInfo(info: String, tag: String = "abc") {
         Log.i(tag, info)
     }
 }
