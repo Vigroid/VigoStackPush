206aa30f19b9d1c521b89a92207da59bd8c25390
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-03-31 14:42:57 +0800

    加上hotObservalbe, 使用PublishSubject实现。优化demo代码实现

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -3,23 +3,38 @@ package com.liulishuo.vigostackpush.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.liulishuo.vigostackpush.R
 import com.liulishuo.vigostackpush.rx.HotnColdObservaleDemo
-import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var observable: Observable<Long> = subscribeHot()
+    private var disposables: MutableList<Disposable> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            subscribeCold()
+            clearDisposable()
+            observable = subscribeCold()
+            Toast.makeText(this@MainActivity, "switch to cold", Toast.LENGTH_SHORT).show()
         }
 
         button2.setOnClickListener {
-            subscribeHot()
+            clearDisposable()
+            observable = subscribeHot()
+            Toast.makeText(this@MainActivity, "switch to hot", Toast.LENGTH_SHORT).show()
+        }
+
+        start.setOnClickListener {
+            disposables.add(observable.subscribe { logInfo(it.toString()) })
+            disposables.add(observable.subscribe { logInfo(it.toString(), "xyz") })
         }
     }
 
@@ -27,18 +42,33 @@ class MainActivity : AppCompatActivity() {
         Log.i(tag, info)
     }
 
-    private fun subscribeCold() {
-        val observable = HotnColdObservaleDemo
+    private fun subscribeCold(): Observable<Long> {
+        return HotnColdObservaleDemo
                 .getColdObservable()
-        observable.subscribe { logInfo(it.toString()) }
-        observable.subscribe { logInfo(it.toString(), "xyz") }
     }
 
-    private fun subscribeHot() {
+    private fun subscribeHot(): Observable<Long> {
         val observable = HotnColdObservaleDemo
                 .getHotObservable()
         observable.connect()
-        observable.subscribe { logInfo(it.toString()) }
-        observable.subscribe { logInfo(it.toString(), "xyz") }
+
+        return observable
+    }
+
+    private fun subscribeHotByPublishSubject(): Observable<Long> {
+        val publishSubject = PublishSubject.create<Long>()
+        //publishSubject做中转，cold observable转hot
+        HotnColdObservaleDemo
+                .getColdObservable()
+                .subscribe(publishSubject)
+        return publishSubject
+    }
+
+    private fun clearDisposable(){
+        for (disposable in disposables){
+            disposable.dispose()
+        }
+
+        disposables.clear()
     }
 }
