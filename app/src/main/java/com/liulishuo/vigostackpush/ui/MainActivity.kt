5436e0797a9a80a7876b60d2d34c7bb75341bc3a
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-04-01 17:27:21 +0800

    map和flatMap操作符

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -4,24 +4,38 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.liulishuo.vigostackpush.R
+import com.liulishuo.vigostackpush.rx.model.Addr
+import com.liulishuo.vigostackpush.rx.model.User
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
+
+    val userList = listOf(User("vigo", listOf(Addr("1 rd"), Addr("524 broadway"))), User("test", listOf(Addr("4 st"), Addr("666 ave"))))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start.setOnClickListener {
-            val startTime = System.currentTimeMillis()
+        button.setOnClickListener {
             Observable
-                    .interval(500, TimeUnit.MILLISECONDS)
-                    .take(5)
-                    .repeatUntil { System.currentTimeMillis() - startTime > 5000 }
-                    .subscribe({ s -> logInfo(s.toString()) }, { t -> logInfo(t.toString()) }, { logInfo("on completed") })
+                    .fromIterable(userList)
+                    .flatMap { usr -> Observable.fromIterable(usr.addrs) }
+                    .subscribe { addr ->
+                        logInfo(addr.street)
+                    }
+        }
+
+        button2.setOnClickListener {
+            Observable
+                    .fromIterable(userList)
+                    .map { usr -> usr.addrs }
+                    .subscribe { addrs ->
+                        for (addr in addrs) {
+                            logInfo(addr.street)
+                        }
+                    }
         }
     }
 
