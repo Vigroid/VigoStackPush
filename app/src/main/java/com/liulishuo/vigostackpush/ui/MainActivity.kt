f43ef4eabfb6e72fb57a2eb0930b51449f738c81
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-04-14 15:16:57 +0800

    buffer的用法

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.liulishuo.vigostackpush.ui
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.liulishuo.vigostackpush.R
-import com.liulishuo.vigostackpush.rx.model.Addr
-import com.liulishuo.vigostackpush.rx.model.User
-import com.liulishuo.vigostackpush.webview.WebViewActivity
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,26 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             Observable
-                    .range(1, 8)
-                    .groupBy { return@groupBy if (it % 2 == 0) "奇数" else "偶数" }
-                    .subscribe { logInfo(it.key) }
+                    .range(1, 11)
+                    .buffer(5)
+                    .subscribe { logInfo(it.toString()) }
         }
 
         button2.setOnClickListener {
             Observable
-                    .range(1, 8)
-                    .groupBy { return@groupBy if (it % 2 == 0) "奇数" else "偶数" }
-                    .subscribe { newSource ->
-                        if (newSource.key == "奇数"){
-                            newSource.subscribe { logInfo("我是奇数欧 $it") }
-                        } else{
-                            newSource.subscribe { logInfo("我是偶数 $it", "xyz") }
-                        }
-                    }
-        }
-
-        btn_web.setOnClickListener {
-            startActivity(Intent(this@MainActivity, WebViewActivity::class.java))
+                    .range(1, 11)
+                    .buffer(5, 1)
+                    .subscribe { logInfo(it.toString()) }
         }
     }
 
