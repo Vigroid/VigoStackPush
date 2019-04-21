72b34ebe5b1169804f64c7b7453a0c9aae84c377
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-04-22 01:34:17 +0800

    join/combineLatest用法和区别

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import com.liulishuo.vigostackpush.R
 import io.reactivex.Observable
 import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
@@ -16,20 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val source1 = Observable.intervalRange(0, 9, 0, 1, TimeUnit.SECONDS)
-        val source2 = Observable.intervalRange(100, 9, 1, 1, TimeUnit.SECONDS)
+        val source1 = Observable.just(1,2,3)
+        val source2 = Observable.just(4,5,6)
 
         button.setOnClickListener {
-            Observable
-                    .merge(source1, source2)
-                    .subscribe { logInfo("$it") }
+            source1
+                    .join(source2,
+                            //source1的时间窗口，这个时间窗口内的source2发过来的数据都组合起来
+                            Function<Int,Observable<Int>> { integer-> Observable.just(integer).delay(200, TimeUnit.MILLISECONDS)},
+                            //source2的时间窗口，这个时间窗口内的source1发过来的数据都组合起来
+                            Function<Int,Observable<Int>> { integer-> Observable.just(integer).delay(200, TimeUnit.MILLISECONDS)},
+                            BiFunction<Int, Int,String>{integer1, integer2 -> "$integer1:$integer2"})
+                    .subscribe { logInfo(it) }
         }
 
         button2.setOnClickListener {
             Observable
-                    .zip(source1, source2,
-                            BiFunction<Long, Long, Long> { t1, t2 -> t1 + t2 })
-                    .subscribe { logInfo("$it") }
+                    .combineLatest(source1, source2,
+                            BiFunction<Int, Int, String> { t1, t2 -> "$t1:$t2" })
+                    .subscribe { logInfo(it) }
         }
     }
 
