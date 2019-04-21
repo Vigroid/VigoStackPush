5e43f5de819a8753705a69561fc338d44bf87f15
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-04-22 01:41:51 +0800

    startWith用法

--- app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/ui/MainActivity.kt
@@ -22,24 +22,14 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             source1
-                    .join(source2,
-                            //source1的时间窗口，这个时间窗口内的source2发过来的数据都组合起来
-                            Function<Int,Observable<Int>> { integer-> Observable.just(integer).delay(200, TimeUnit.MILLISECONDS)},
-                            //source2的时间窗口，这个时间窗口内的source1发过来的数据都组合起来
-                            Function<Int,Observable<Int>> { integer-> Observable.just(integer).delay(200, TimeUnit.MILLISECONDS)},
-                            BiFunction<Int, Int,String>{integer1, integer2 -> "$integer1:$integer2"})
+                    .startWith(source2)
+                    .startWith(Int.MIN_VALUE)
                     .subscribe { logInfo(it) }
         }
 
-        button2.setOnClickListener {
-            Observable
-                    .combineLatest(source1, source2,
-                            BiFunction<Int, Int, String> { t1, t2 -> "$t1:$t2" })
-                    .subscribe { logInfo(it) }
-        }
     }
 
-    private fun logInfo(info: String?, tag: String = "abc") {
-        Log.i(tag, info)
+    private fun logInfo(info: Any?, tag: String = "abc") {
+        Log.i(tag, "$info")
     }
 }
