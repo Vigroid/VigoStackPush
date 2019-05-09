210530897f49e0c3e0244e260d28706fb631ce79
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-05-10 00:41:43 +0800

    背压ANR的exmaple

--- app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
@@ -5,26 +5,24 @@ import android.os.Bundle
 import com.liulishuo.core.logInfo
 import com.liulishuo.vigostackpush.R
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_rx.*
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 class RxActivity :Activity(){
-    val sdf = SimpleDateFormat("HH:mm:ss")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx)
 
-        val source = Observable.interval(1, TimeUnit.SECONDS).take(10).replay()
-
-        source.connect()
+        val source = Observable.create<Int> {
+            for (i in 0..Integer.MAX_VALUE){
+                it.onNext(i)
+            }
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
 
         button.setOnClickListener {
-            source.subscribe { logInfo("subscriber 1 - $it - time: ${sdf.format(Date())}") }
-            source.delaySubscription(3, TimeUnit.SECONDS)
-                    .subscribe { logInfo("subscriber 2 - $it - time: ${sdf.format(Date())}") }
+            source.subscribe { logInfo(it) }
         }
     }
 }
