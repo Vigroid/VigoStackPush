4a58d74886497bc382b19584bf9eb58721d4de97
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-05-10 01:00:36 +0800

    使用ERROR背压策略

--- app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
@@ -4,25 +4,28 @@ import android.app.Activity
 import android.os.Bundle
 import com.liulishuo.core.logInfo
 import com.liulishuo.vigostackpush.R
-import io.reactivex.Observable
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_rx.*
 
-class RxActivity :Activity(){
+class RxActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx)
 
-        val source = Observable.create<Int> {
-            for (i in 0..Integer.MAX_VALUE){
-                it.onNext(i)
-            }
-        }.subscribeOn(Schedulers.io())
+        val source = Flowable
+                .create<Int>({
+                    for (i in 0..999) {
+                        it.onNext(i)
+                    }
+                }, BackpressureStrategy.ERROR)
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
 
         button.setOnClickListener {
-            source.subscribe { logInfo(it) }
+            source.subscribe({ logInfo(it) }, { ex -> logInfo(ex) })
         }
     }
 }
