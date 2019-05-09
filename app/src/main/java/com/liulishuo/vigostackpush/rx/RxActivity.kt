0286bb220c189d9adf1dab63c761e5a80b8cfe2a
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-05-10 00:26:55 +0800

    replay用法

--- app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/rx/RxActivity.kt
@@ -6,20 +6,25 @@ import com.liulishuo.core.logInfo
 import com.liulishuo.vigostackpush.R
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_rx.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class RxActivity :Activity(){
+    val sdf = SimpleDateFormat("HH:mm:ss")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx)
 
-        val source1 = Observable.just(1,2,3)
-        val source2 = Observable.just(4,5,6)
+        val source = Observable.interval(1, TimeUnit.SECONDS).take(10).replay()
+
+        source.connect()
 
         button.setOnClickListener {
-            source1
-                    .startWith(source2)
-                    .startWith(Int.MIN_VALUE)
-                    .subscribe { logInfo(it) }
+            source.subscribe { logInfo("subscriber 1 - $it - time: ${sdf.format(Date())}") }
+            source.delaySubscription(3, TimeUnit.SECONDS)
+                    .subscribe { logInfo("subscriber 2 - $it - time: ${sdf.format(Date())}") }
         }
     }
 }
