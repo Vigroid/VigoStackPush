0e7c36150c3c2c37625e2dc33fac78843795828a
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-05-10 16:21:13 +0800

    同步的走完两个挂起函数

--- app/src/main/java/com/liulishuo/vigostackpush/coroutine/CoroutineActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/coroutine/CoroutineActivity.kt
@@ -6,27 +6,32 @@ import com.liulishuo.core.logInfo
 import com.liulishuo.vigostackpush.R
 import kotlinx.android.synthetic.main.activity_coroutine.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class CoroutineActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine)
 
-        var job: Job?
         btn_test.setOnClickListener {
             runBlocking {
-                withTimeout(3000) {
-                    repeat()
+                val time = measureTimeMillis {
+                    val one = function1()
+                    val two = function2()
+                    logInfo("The answer is ${one+ two}")
                 }
-                logInfo("yo")
+                logInfo("time cost is $time")
             }
         }
     }
 
-    private suspend fun repeat() {
-        repeat(1000) {
-            logInfo("$it")
-            delay(500)
-        }
+    private suspend fun function1(): Int{
+        delay(1000)
+        return 13
+    }
+
+    private suspend fun function2(): Int{
+        delay(1000)
+        return 29
     }
 }
