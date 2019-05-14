8b3e40386a496ac17429c7dc439a25a7f27e81a4
weikang.yang01 <weikang.yang01@liulishuo.com>
2019-05-14 16:15:32 +0800

    GlobalScope的coroutine独立，不会受父coroutine cancel影响

--- app/src/main/java/com/liulishuo/vigostackpush/coroutine/CoroutineActivity.kt
+++ app/src/main/java/com/liulishuo/vigostackpush/coroutine/CoroutineActivity.kt
@@ -2,6 +2,7 @@ package com.liulishuo.vigostackpush.coroutine
 
 import android.app.Activity
 import android.os.Bundle
+import com.liulishuo.core.logInfo
 import com.liulishuo.vigostackpush.R
 import kotlinx.android.synthetic.main.activity_coroutine.*
 import kotlinx.coroutines.*
@@ -13,20 +14,29 @@ class CoroutineActivity : Activity() {
         setContentView(R.layout.activity_coroutine)
 
         btn_test.setOnClickListener {
-            newSingleThreadContext("Ctx1").use { ctx1 ->
-                newSingleThreadContext("Ctx2").use { ctx2 ->
-                    runBlocking(ctx1) {
-                        log("Started in ctx1, ${coroutineContext[Job]}")
-                        withContext(ctx2) {
-                            log("Working in ctx2, ${coroutineContext[Job]}")
-                        }
-                        log("Back to ctx1, ${coroutineContext[Job]}")
+            runBlocking {
+                // launch a coroutine to process some kind of incoming request
+                val request = launch {
+                    // it spawns two other jobs, one with GlobalScope
+                    GlobalScope.launch {
+                        logInfo("job1: I run in GlobalScope and execute independently!")
+                        delay(1000)
+                        logInfo("job1: I am not affected by cancellation of the request")
+                    }
+                    // and the other inherits the parent context
+                    launch {
+                        delay(100)
+                        logInfo("job2: I am a child of the request coroutine")
+                        delay(1000)
+                        logInfo("job2: I will not execute this line if my parent request is cancelled")
                     }
                 }
+                delay(500)
+                request.cancel() // cancel processing of the request
+                delay(1000) // delay a second to see what happens
+                logInfo("main: Who has survived request cancellation?")
             }
         }
 
     }
-
-    fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 }
